50fbf815b9a8aa2660425ef238b1838cc1763fd2
surielisrodriguez <surielisrodriguez7@gmail.com>
2023-04-30 23:26:42 -0700

    assesment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,24 +10,21 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
-
-// Pseudo code:
-
-// a) Create a test with expect statements for each of the variables provided.
-
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
-
-// b) Create the function that makes the test pass.
-
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // Pseudo code:
+//input: object
+//output: whether number is divisible by 3
+// process: using the module operator we can use a conditional saying that if the number is divisible by three then we want it to display that to the URLSearchParams. With our else statement we can tell it to catch the rest of the numbers and give a different output. 
 
 // a) Create a test with expect statements for each of the variables provided.
+descibe("testing whether number is divisible by three",()=>{
+    it("returns whether number is divisible by three",() =>{
+        expect(divisibleBy(object1).toEqual("15 is divisible by three"))
+        expect(divisibleBy(object2).toEqual("0 is divisible by three"))
+        expect(divisibleBy(object3).toEqual("-7 is not divisible by three"))
+    })
+})
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -38,11 +35,33 @@ const object3 = { number: -7 }
 
 // b) Create the function that makes the test pass.
 
+const divisibleBy = (object) => {
+    if (object % 3 === 0 ) {
+        return `${object.number} is divisible by three.`
+    }else {
+        return `${object.number} is not divisible by 3`
+    }
+}
+console.log (divisibleBy(object1));
+console.log (divisibleBy(object2));
+console.log (divisibleBy(object3));
+
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // Pseudo code:
-
+//input: string
+//output: modified string
+//process: using a fucntion we can tell this program to take the string as an inout and return it with a capitalized letter. using .map to iterate through array. Then using the charAt we can tell the program to look at a specified value, then apply .toUpperCase to capitalize, and using .slice and telling it where to slice and stop the uppercasing process.
 // a) Create a test with expect statements for each of the variables provided.
+
+
+describe("returns array with the words capitalized", () => {
+    it("returns array with words capitalized", () => {
+    expect(allCaps(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]);
+    expect(allCaps(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"]);
+})
+})
+
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -51,11 +70,31 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // b) Create the function that makes the test pass.
 
+const allCaps = (array) => {
+    return array.map((element) => {
+        return element.charAt(0).toUpperCase() + element.slice(1)
+    })
+}
+console.log(allCaps(randomNouns1))
+console.log(allCaps(randomNouns2))
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
-// Pseudo code:
+// Pseudo code: 
+// Input: String
+// Output: index of first vowel in String
+// Process: using a for loop we tell the program to examine our string starting from the zero index and going up. Then using a conditional and .includes we tell it to check for vowels in our string and tell it to return the index if found.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+
+describe('indexVowel function', () => {
+    it('should return the index of the first vowel in a string', () => {
+      expect(indexVowel("learn")).toBe(1);
+      expect(indexVowel("academy")).toBe(0);
+      expect(indexVowel("challenges")).toBe(2);
+    });
+  });
+  
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -63,5 +102,16 @@ const vowelTester2 = "academy"
 // Expected output: 0
 const vowelTester3 = "challenges"
 // Expected output: 2
+const threeStrings = vowelTester1 + vowelTester2 + vowelTester3
 
 // b) Create the function that makes the test pass.
+const indexVowel = (element) => {
+    for (let i = 0; i < element.length; i++) {
+      if ("aeiou".includes(element[i])) {
+        return i;
+      }
+    }
+  }
+console.log(indexVowel(vowelTester1));
+console.log(indexVowel(vowelTester2));
+console.log(indexVowel(vowelTester3));
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: A paramenter is something we set in a function to deliniate what element were going to be working with, and arugument is the log we are passing 
 
-Researched answer:
+Researched answer: An  Argument is a value passed to a function/method. A parameter ia a varibale that is defined in a method/function declaration. Parameters are placeholder values, and the arguments is the actual values that will be used by the method/function to do the programmed task.
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer:  I believe it holds the current value, the array and the index. The only required value is the current value being used for the array.
 
-Researched answer:
+Researched answer:.map has three values it can take in which is the current value in the array, index which is the index of the element in the array, and the array which is the array being used by the .map. The only one required is the current value being used. 
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer:.map will return an array of the same lenght while .filter filters out elements within the array based on the logic given.
 
-Researched answer:
+Researched answer: I was wrong because they are simliar since they both return new arrays. Filter only displays the elements in the array that pass a specified condition. Map changes all the elemts in the array using the callback fucntion. 
 
-1. What is iteration?
+4. What is iteration?
 
-Your answer:
+Your answer: An iteration is how many times we tell the computer to run a loop in our program
 
-Researched answer:
+Researched answer: I was corect, the Iteration is going to t=be the process of running and repeating a sequence a specified amount of times. Its purpose is to easily perform repetetive tasks. 
 
-1. What is the difference between a function and a method?
+5. What is the difference between a function and a method?
 
-Your answer:
+Your answer: A built in method is an existing line of code that has a specific function it does. A function is a structure that we can add logic to and get it to perform a specific task or tasks. 
 
-Researched answer:
+Researched answer: Fucntion can be astand alone object can that can be called/used in any point of you rprogram, a methis is a fucntion that is tied to a class or object. 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: I do not know what hoising is. 
 
-Researched answer:
+Researched answer: Hoisting is a way to be able access functions/variables before you declare them in your program. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Component:
+1. Component: Reuseable code block that has a specific function to make advanced user interfaces. 
 
-2. Spread operator:
+2. Spread operator: this operator is represented by three dots (...) and is used to indicate you want to spread the elements and make a new array.
 
-3. React state:
+3. React state: this is used to managae what a data a component will repond to, and it represents the current state of component.
 
-4. React props:
+4. React props: this is in reference to properties and can be used to pass data from the parent component to the child component, which cannot make or modify data.
 
-5. DOM events:
+5. DOM events: DOM is the Document object model and dom events are actions that a user can touch on a web page. 
